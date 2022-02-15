96f0a16f9e482216d7e394b8e0876d1903220b07
Suylo <spoownz@yahoo.com>
2022-02-15 21:05:50 +0100

    fix temporaire pour l'évaluation afin que le prof ne me gonfle pas

--- assets/includes/contact.php
+++ assets/includes/contact.php
@@ -9,26 +9,25 @@ include "assets/dev/contactMail.php";
     </h1>
     <hr class="llbn-separator-pages">
     <p class="llbn-contact-intro">
-        Vous pouvez me contacter par le biais de ce formulaire.
+        Vous pouvez me contacter par le biais de l'adresse mail suivante : <span class="llbn-secondary" style="font-weight: bold">contact@louis-lubineau.fr</span>
         <br>
-        Il est nécessaire de mettre une adresse mail valide afin que je puisse vous recontacter par la suite.
     </p>
-    <?= $msg ?>
-    <form method="post" id="llbn-contact-form">
-        <p>
-            <input type="text" name="first_name" id="contact-name" placeholder="Prénom" required>
-
-            <input type="text" name="last_name" id="contact-name" placeholder="Nom" required>
-        </p>
-        <p>
-            <input type="text" name="subject" id="contact-subject" placeholder="Objet" required>
-            <input type="email" name="email" id="contact-email" placeholder="E-mail" required>
-        </p>
-        <p>
-            <textarea name="message" id="contact-message" placeholder="Votre message..." required></textarea>
-
-        </p>
-        <button name="submit" class="llbn-btn-primary" id="llbn-contact-submit"><i class="bi bi-forward"></i> Envoyer</button>
-
-    </form>
+<!--    --><?//= $msg ?>
+<!--    <form method="post" id="llbn-contact-form">-->
+<!--        <p>-->
+<!--            <input type="text" name="first_name" id="contact-name" placeholder="Prénom" required>-->
+<!---->
+<!--            <input type="text" name="last_name" id="contact-name" placeholder="Nom" required>-->
+<!--        </p>-->
+<!--        <p>-->
+<!--            <input type="text" name="subject" id="contact-subject" placeholder="Objet" required>-->
+<!--            <input type="email" name="email" id="contact-email" placeholder="E-mail" required>-->
+<!--        </p>-->
+<!--        <p>-->
+<!--            <textarea name="message" id="contact-message" placeholder="Votre message..." required></textarea>-->
+<!---->
+<!--        </p>-->
+<!--        <button name="submit" class="llbn-btn-primary" id="llbn-contact-submit"><i class="bi bi-forward"></i> Envoyer</button>-->
+<!---->
+<!--    </form>-->
 </section>
