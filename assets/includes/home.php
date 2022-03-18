86d748e2532fbb8330b99a13455d58eead8d2371
Suylo <spoownz@yahoo.com>
2022-03-18 01:07:18 +0100

    Home btn deleted

--- assets/includes/home.php
+++ assets/includes/home.php
@@ -7,7 +7,6 @@
         <p>
             Sur ce site, vous pouvez y trouver les <span class="llbn-primary">compétences</span> que j'ai acquises au cours de mes <span class="llbn-primary">formations</span> ou encore des choses diverses et variées qui ont un lien avec mes <span class="llbn-primary">études</span>.
             <br><br>
-            Vous pouvez commencer la visite en cliquant sur le bouton ci-dessous ou bien utiliser le menu à gauche. <span class="llbn-primary">Bonne visite !</span>
+            Vous pouvez commencer la visite en utilisant le menu à gauche. <span class="llbn-primary">Bonne visite !</span>
         </p>
-        <a class="llbn-btn-primary" href="introduction"><i class="bi bi-person"></i> Présentation</a>
         </section>
