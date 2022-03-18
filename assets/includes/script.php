4f9c04a384597a0ebae7cf3bcb7080fe1daa3477
Suylo <spoownz@yahoo.com>
2022-03-18 01:19:08 +0100

    New src added

--- assets/includes/script.php
+++ assets/includes/script.php
@@ -1,3 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 <script src="assets/js/particles.js"></script>
+<script src="assets/js/dynamicLoad.js"></script>
