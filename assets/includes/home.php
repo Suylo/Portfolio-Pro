7d418bddaf2369bbf57c5033995eda29ab94c38b
Suylo <spoownz@yahoo.com>
2022-06-06 17:28:42 +0200

    Sync with server

--- assets/includes/home.php
+++ assets/includes/home.php
@@ -1,12 +1,21 @@
 <section id="llbn-title">
-        <h1>
-            <span class="llbn-primary">B</span>ienvenue sur mon
-            <span class="llbn-text-cursive llbn-primary">Portfolio</span>
-        </h1>
-        <hr class="llbn-separator">
-        <p>
-            Sur ce site, vous pouvez y trouver les <span class="llbn-primary">compétences</span> que j'ai acquises au cours de mes <span class="llbn-primary">formations</span> ou encore des choses diverses et variées qui ont un lien avec mes <span class="llbn-primary">études</span>.
-            <br><br>
-            Vous pouvez commencer la visite en utilisant le menu à gauche. <span class="llbn-primary">Bonne visite !</span>
-        </p>
-        </section>
+    <h1>
+        <span class="llbn-primary">B</span>ienvenue sur mon
+        <span class="llbn-text-cursive llbn-primary">Portfolio</span>
+    </h1>
+    <hr class="llbn-separator">
+    <p>
+        À travers ce site, vous pourrez y trouver les <span class="llbn-primary">compétences</span>
+        que j'ai acquises ces dernières années (personnellement et professionnellement), ainsi que toutes les <span class="llbn-primary">formations</span>  que j'ai suivies et tous les <span class="llbn-primary">projets réalisés</span> dans celles-ci.
+        <br>
+        <br>
+        Si vous avez des <span class="llbn-primary">suggestions</span> concernant le site ou alors si vous souhaitez seulement me contacter, rendez-vous sur la page "<span class="llbn-primary">Contact</span>" !
+
+        <br>
+        <br>
+        J'ai réalisé ce site en partant de <span class="llbn-primary">zéro</span>, sans framework CSS, ni utilisé de template ! De plus, j'ai utilisé de l'<span class="llbn-primary">Ajax</span> afin de permettre un chargement rapide des pages depuis une <span class="llbn-primary">base de données</span>.
+        <br>
+        <br>
+        <span class="llbn-primary">Bonne visite !</span>
+    </p>
+</section>
